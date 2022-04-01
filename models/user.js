c1cd3ef00c1193af9e1e8aa4932037b41d51d0e8
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-04-01 16:28:49 -0400

    Added in the email validator imported from the validator npm package

--- models/user.js
+++ models/user.js
@@ -1,25 +1,26 @@
 // import the mongoose NPM package and store it in a constant variable called mongoose.
-const { assert } = require("console");
 const mongoose = require(`mongoose`);
+// import the validator npm package, specifically the isEmail function to use for the validation of an email address when it is inside of the document.
+const { default: isEmail } = require("validator/lib/isEmail");
+// import out thought
 const thought = require(`./thought`);
 
-
+// create the schema for the users.
 const userSchema = new mongoose.Schema({
+    // username has to exist, must be unique, and is a string.
     username: {
         type: String,
         required: true,
         unique: true,
         trim: true
     },
+    // email has to exist, must be unique, is a string, and must be an email through the validate.
     email: {
         type: String,
         required: true,
         unique: true,
-        // This Custom validator was found on the mongoose js docs to ensure that the information that was provided is an email address
-        validate: {
-            validator: () => Promise.resolve(false),
-            message: `please provide a proper email address`
-        }
+        // run the isEmail function from validator to ensure the section is an email address. if ti comes back as false then the message is given instead
+        validate: [isEmail, `Please provide a valid email address.`]
     },
     thoughts: [thought]
 })
