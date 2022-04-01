5be23b6f94c6c10381547858841396d70cb351d9
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-04-01 18:06:45 -0400

    created the endpoint routes for the user routes and connect them to the specified request handlers from the userController

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,6 +1,28 @@
+// imports the express router package
 const router = require(`express`).Router();
-const { showAllUsers, showOneUser, addNewUser, removeUser, updateUser } = require(`../../controllers/userControl`)
+// imports the request handlers from the controller folder.
+const { showAllUsers, showOneUser, addNewUser, removeUser, updateUser, addFriend, removeFriend } = require(`../../controllers/userControl`)
 
-router.get(`/`)
+// defines the get all users route.
+router.get(`/`, showAllUsers);
 
+// defines the get one user route.
+router.get(`/:id`, showOneUser);
+
+// defines the route to add a new user.
+router.post(`/`, addNewUser);
+
+// defines the route to update a users information.
+router.put(`/`, updateUser);
+
+// defines the route to delete a user by the specified ID.
+router.delete(`/:id`, removeUser);
+
+// defines the route to add a new friend to the chosen user.
+router.post(`/:userId/friends`, addFriend);
+
+// defines the route to remove a chosen friend from the specified user.
+router.delete(`/:userId/friends/:friendId`, removeFriend);
+
+// exports the router so that it can be imported elsewhere in the application.
 module.exports = router;
