23ae534bc61e9902b1973f645f476f3486efd5df
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-04-02 00:50:47 -0400

    Added the bulk fo the functionality for the though conrtroller for the request handlers, the last two to finish are the addreaction and remove reaction to the thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -5,23 +5,43 @@ const { User, Reaction, Thought } = require(`../models`)
 const thoughtRequestHandler = {
     // handles a get request to /api/thoughts
     showAllThoughts(req, res) {
-        res.json(`showallthoughts`)
+        // finds all existing thoughts in the database
+        Thought.find().then((thoughts) => res.json(thoughts))
+            .catch((err) => res.status(500).json(err))
     },
     // handles a get request to /api/thoughts/:id
     showOneThought(req, res) {
-        res.json(`showonethought`)
+        Thought.findOne({ _id: req.params.id }).select(`-__v`)
+            .populate(`reactions`).then((thought) => {
+                !thought ? res.status(404).json({ message: `Thought not found` })
+                    : res.json(thought)
+            }).catch((err) => res.status(500).json(err))
     },
     // handles a post request to /api/thoughts
     addNewThought(req, res) {
-        res.json(`addnewthought`)
-    },
-    // handles a delete request to /api/thoughts/:id
-    removeThought(req, res) {
-        res.json(`removethought`)
+        Thought.create(req.body).then((newThought) => res.json(newThought))
+            .catch((err) => res.status(500).json(err));
     },
     // handles a put request to /api/thoughts/:id
     updateThought(req, res) {
-        res.json(`updatethought`)
+        Thought.findOneAndUpdate({ _id: req.params.id }, req.body,
+            // Tells the client to return the new rendition of the document and to run the validators so no false information can be added
+            { new: true, runValidators: true })
+            // handles any errors if they were to occur
+            .then((updatedThought) => {
+                !updatedThought
+                    ? res.status(404).json({ message: `No Thought found with that ID` })
+                    : res.json(updatedThought)
+            }).catch((err) => res.status(500).json(err));
+    },
+    // handles a delete request to /api/thoughts/:id
+    removeThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.id })
+            .then((deletedThought) => {
+                !deletedThought
+                    ? res.status(404).json({ message: `No Thought found with that ID` })
+                    : res.json({ message: `Thought has been deleted` })
+            }).catch((err) => res.status(500).json(err));
     },
     // handles a post request to /api/thoughts/:thoughtId/reactions
     addReaction(req, res) {
