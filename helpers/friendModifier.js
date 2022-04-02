5e4dc33b7083d4d0ad3e5c08eeb3cf001455858b
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-04-02 00:36:36 -0400

    application now allows for the full functionality of adding and deleting friends, with this the user Controller is done. Next objective is the toughts controller

--- helpers/friendModifier.js
+++ helpers/friendModifier.js
@@ -4,39 +4,24 @@
 const { User } = require("../models")
 
 // create a custom helper function to add / remove target 1 to target 2 and add target 2 to target 1
-function friendModifier(action, target1, target2, res) {
+async function friendModifier(action, target1, target2, res) {
     switch (action) {
         // add a friend
         case `add`:
-            User.findOneAndUpdate(
-                { _id: target1 }, { $addToSet: { friends: target2 } }, { new: true, runValidators: true }
-            ).then((data) => !data
-                ? res.json(404).json({ message: `Check your inputs` })
-                : res.json({ message: `Friend Added` })
-            ).catch((err) => res.status(500).json(err));
-
-            User.findOneAndUpdate(
-                { _id: target2 }, { $addToSet: { friends: target1 } }, { new: true, runValidators: true }
-            ).then((data) => !data
-                ? res.json(404).json({ message: `Check your inputs` })
-                : res.json({ message: `Friend Added` })
-            ).catch((err) => res.status(500).json(err));
+            await User.findOneAndUpdate(
+                { _id: target1 }, { $addToSet: { friends: target2 } }, { new: true, runValidators: true });
+            await User.findOneAndUpdate(
+                { _id: target2 }, { $addToSet: { friends: target1 } }, { new: true, runValidators: true });
+            res.json({ message: `Friends have been added` })
             break;
         // remove a friend
         case `delete`:
-            User.findOneAndUpdate(
-                { _id: target1 }, { $pull: { friends: target2 } }, { new: true, runValidators: true }
-            ).then((data) => !data
-                ? res.json(404).json({ message: `Check your inputs` })
-                : res.json({ message: `Friend removed` })
-            ).catch((err) => res.status(500).json(err));
+            await User.findOneAndUpdate(
+                { _id: target1 }, { $pull: { friends: target2 } }, { new: true, runValidators: true });
 
-            User.findOneAndUpdate(
-                { _id: target2 }, { $pull: { friends: target1 } }, { new: true, runValidators: true }
-            ).then((data) => !data
-                ? res.json(404).json({ message: `Check your inputs` })
-                : res.json({ message: `Friend removed` })
-            ).catch((err) => res.status(500).json(err));
+            await User.findOneAndUpdate(
+                { _id: target2 }, { $pull: { friends: target1 } }, { new: true, runValidators: true });
+            res.json({ message: `Friends have been removed` })
             break;
         // only will occur is the function is not called properly
         default: res.json({ message: `something went wrong` })
